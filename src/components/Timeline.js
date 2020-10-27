927ed20c6ec4488f27d02ae492173f30323c9fd2
thaliadettenborn <thalia.born@gmail.com>
2020-10-27 18:10:29 -0300

    adding component create a post

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,19 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Header from './Header';
+import CreatePost from './CreatePost';
 
 export default function Timeline() {
-    
-
     return (
-        <Container>
+        <>
             <Header />
-        </Container>
+            <Container>
+                <CreatePost />
+            </Container>
+            {/* hashtags */}
+        </>
     );
 }
 
 const Container = styled.div`
-    background: #333333;
-    width: 100vw;
+    width: 610px;
     height: 100vh;
+    margin: 95px 420px 0 auto;
 `;
