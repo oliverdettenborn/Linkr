254e1103be6a7c71efa637b05bddbbf0c547a1ab
thiagoribb <thiribeiro142@gmail.com>
2020-10-27 23:31:42 -0300

    add loading message - Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useContext, useState } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
+import styled from 'styled-components';
 
-import Header from './Header';
 import Post from './Post';
 import UserContext from '../context/UserContext';
 import StylePages from './StylePages';
@@ -11,20 +10,37 @@ import CreatePost from './CreatePost';
 export default function Timeline() {
     const { user } = useContext(UserContext);
     const { token } = user;
-    const [posts, setPosts] = useState([]);
+    const [ posts, setPosts ] = useState([]);
+    const [loading,setLoading] = useState(false);
 
     useEffect(() => {
         const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/posts?offset=0&limit=2", {headers: {'user-token': token}});
 
         request.then(reply => {
             setPosts(reply.data.posts);
+            setLoading(false);
         });
+
+        request.catch(err => {
+            alert('Houve uma falha ao obter os posts, por favor atualize a página');
+          })
     },[]);
 
     return (
         <StylePages title='timeline'>
             <CreatePost />
-            {posts.map( p => <Post post={p} key={p.id}/> )}
+
+            {loading 
+                ? <Message>Loading...</Message>
+                : posts.length == 0 
+                    ? <Message>Nenhum post foi encontrado.</Message>
+                    : posts.map( p => <Post post={p} key={p.id}/> )
+            }
         </StylePages>  
     );
 }
+
+const Message = styled.div`
+    color: red;
+`;
+
