363d09159db82fa7bd5ae8f07ebae49fe55b36a4
thaliadettenborn <thalia.born@gmail.com>
2020-10-29 20:31:49 -0300

    adding scroll infinite at my-likes page

--- src/components/LikesTimeline.js
+++ src/components/LikesTimeline.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext, useState } from 'react';
 import Post from './Post';
 import axios from 'axios';
 import styled from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import UserContext from '../context/UserContext';
 import StylePages from './StylePages';
@@ -9,33 +10,44 @@ import Load from './Load'
 
 export default function LikesTimeline() {
     const { user } = useContext(UserContext);
-    const [loading,setLoading] = useState(false);
+    const [loading,setLoading] = useState(true);
     const [likedPosts, setLikedPosts] = useState([]);
-
+    const [hasMore,setHasMore] = useState(true);
+    const [offset,setOffset] = useState(0);
     useEffect(() => {
-      setLoading(true);
-      const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/posts/liked", {headers: {'user-token': user.token}});
+        const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/posts/liked", {headers: {'user-token': user.token}});
 
-      request.then(reply => {
-          setLikedPosts(reply.data.posts);
-          setTimeout(() => {
-              setLoading(false);
-            }, 1000);
-      });
+        request.then(reply => {
+            setLikedPosts([...posts,...reply.data.posts])
+            setLoading(false);
+        });
 
-      request.catch(err => {
-          alert('Houve uma falha ao obter os posts, por favor atualize a página');
+        request.catch(err => {
+            alert('Houve uma falha ao obter os posts, por favor atualize a página');
         })
-    },[]);
+    },[offset]);
+
+    function handleLoader(){
+        setHasMore(false);
+        setOffset(offset+10);
+    }
 
     return (
         <StylePages title='likes'>
-          {loading 
-                ? <Load />
-                : likedPosts.length === 0 
-                    ? <Message>Nenhum post foi encontrado.</Message>
-                    : likedPosts.map( p => <Post post={p} key={p.id}/> )
-          }
+            <InfiniteScroll
+                pageStart={offset}
+                loadMore={handleLoader}
+                hasMore={hasMore}
+                loader={<Load />}
+            >
+                {
+                    loading
+                    ? <Load />
+                    : likedPosts.length === 0 
+                        ? <Message>Nenhum post foi encontrado.</Message>
+                        : likedPosts.map( p => <Post post={p} key={p.id}/> )
+                }
+            </InfiniteScroll>
         </StylePages>  
     );
 }
